e2f3bc6bb6c6403981f4e53d618c0600bc048f3d
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-03-01 23:24:20 -0500

    python-p3-organizing-bears-lab/
    python-p3-organizing-bears-lab/

--- python-p3-organizing-bears-lab-main/lib/insert.sql
+++ python-p3-organizing-bears-lab-main/lib/insert.sql
@@ -1,15 +1,9 @@
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Mr. Chocolate', 4, 'M', 'brown', 'docile', True);
-
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Rowdy', 5, 'M', 'black', 'mean', True);
-
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Tabitha', 6, 'F', 'gray', 'loving', True);
-
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Sergeant Brown', 7, 'M', 'brown', 'asshole',False);
-
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Melissa', 8, 'F', 'black', 'caring', True);
-
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Grinch', 9, 'M', 'brown', 'grumpy', True);
-
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Wendy', 10, 'F', 'gray', 'docile', True);
-
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(NULL, 11, 'F', 'brown', 'docile', True);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sergeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
