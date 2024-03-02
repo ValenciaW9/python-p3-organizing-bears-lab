f042358df3624d850a84c85859ed41ca634b7472
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-03-01 23:33:28 -0500

    python-p3-organizing-bears-lab/
    python-p3-organizing-bears-lab/

--- python-p3-organizing-bears-lab-main/lib/sql_queries.py
+++ python-p3-organizing-bears-lab-main/lib/sql_queries.py
@@ -1,32 +1,18 @@
 select_all_female_bears_return_name_and_age = """
-   SELECT name,age
-   FROM bears
-   WHERE sex='F'
-
+    SELECT name, age FROM bears WHERE sex = 'F'
 """
 
 select_all_bears_names_and_orders_in_alphabetical_order = """
-     SELECT name
-   FROM bears
-   ORDER BY name
+    SELECT name FROM bears ORDER BY name
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-   SELECT name, age
-    FROM bears
-    WHERE alive = 1
-    ORDER BY age;
+    SELECT name, age FROM bears WHERE alive = 1 ORDER BY age
 """
 
 select_oldest_bear_and_returns_name_and_age = """
-     SELECT name, age
-    FROM bears
-    ORDER BY age DESC
-    LIMIT 1;
+    SELECT name, age FROM bears ORDER BY age DESC LIMIT 1
 """
 select_youngest_bear_and_returns_name_and_age = """
-   SELECT name, age
-    FROM bears
-    ORDER BY age
-    LIMIT 1;
+    SELECT name, age FROM bears ORDER BY age LIMIT 1
 """
